ce52f94455591b018f12ade703d1b7d686c6caf4
Efanin <efanin.igor@gmail.com>
2024-03-27 16:11:11 +0300

    string

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,3 +1,19 @@
 ﻿using Project;
+using Bytescout.Spreadsheet;
 
-Programm3.main();
+Spreadsheet doc = new Spreadsheet();
+doc.LoadFromFile("C:\\Users\\efani\\Source\\Repos\\Efanin\\Project\\Project\\excel-spreadsheet-examples-for-students.xlsx");
+Worksheet sheet = doc.Workbook.Worksheets[0];
+string[,] data = new string[sheet.NotEmptyRowMax + 1, sheet.NotEmptyColumnMax + 1];
+for (int i = 0; i < sheet.NotEmptyRowMax+1; i++)
+    for (int j = 0; j < sheet.NotEmptyColumnMax+1; j++)
+        data[i,j] =  sheet.Cell(i, j).ToString();
+
+for (int i = 0; i < data.GetLength(0); i++)
+{
+    for (int j = 0; j < data.GetLength(1); j++)
+    {
+        Console.Write(data[i,j]+" ");
+    }
+    Console.WriteLine();
+}
---
+++ Project/Work.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    static class Work
+    {
+        public static void main()
+        {
+            string word = "wello word".Print();
+            word.Contains("word").Print();// определяет, содержится ли подстрока в строке
+            (word + " !!!").Print();
+            word.IndexOf("wo").Print();
+            word.Insert(5, "     ").Print();
+            word.Replace("word", "world").Print();
+            string text = "И поэтому все так произошло";
+            text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray().Print();
+            word.Remove(6).Print();
+            word[6..^0].Print();
+            text.Replace("И поэтому все так ", "").Print();
+        }
+    }
+}
